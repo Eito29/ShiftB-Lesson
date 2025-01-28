413cf1c1829b74140680e72238a09eeffbe772b4
Eito29 <fukutani.eito8@gmail.com>
2025-01-28 16:26:32 +0900

    課題3の実装

--- src/pages/Report.jsx
+++ src/pages/Report.jsx
@@ -1,19 +1,33 @@
 import React from 'react'
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { posts } from '../data/posts'
 import classes from '../styles/Top.module.css'
 
 export default function Report() {
   // useParamsでURLパラメータを取得。
   const { id } = useParams(); // { id }は<Route path="/report/:id" element={<Report />} />と一致
+  const [post, setPosts] = useState([]); // 「空の配列」
+  const [isLoading, setIsLoading] = useState(true); // 読み込み中用に準備。isLoadingの初期状態をtrueに設定
 
-  // idに基づいて該当する投稿データを取得
-  // findで条件にあった最初のデータを見つける。
-  // idは文字列として取得されるため、parseIntで数値に変換
-  const post = posts.find(post => post.id === parseInt(id));
+  // APIでpostsを取得する処理をuseEffectで実行。
+  useEffect(() => { //データを非同期に取得するため、最初のレンダリング時にはpostはまだnullかundefined
+    const loadPosts = async () => {
+      const res = await fetch(`https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts/${id}`);
+      const data = await res.json();
+      setPosts(data.post);
+      setIsLoading(false); // データ取得後に読み込み中の状態を終了する
+    }
 
-  // 記事がない場合はこれを表示
-  if (!post) return <div>記事が見つかりません</div>;
+    loadPosts();
+  }, [id]) // idの値が変更されるたびにuseEffectが発火
+
+  // 読み込み中
+  if (isLoading) {
+    return <div>読み込み中です。しばらくお待ちください。</div>;
+  }
+
+  // 記事がない場合
+  if (!post) return <div>記事が見つかりません。</div>; // この部分がないと、postがまだデータを取得できていない状態でレンダリングが進んでしまい、エラーが発生する
 
   return (
     <div>
--- src/pages/Top.jsx
+++ src/pages/Top.jsx
@@ -1,14 +1,27 @@
 import React from 'react'
-import { posts } from '../data/posts'
-import { Link } from "react-router-dom";
+import { useEffect, useState } from 'react'
+import { Link } from "react-router-dom"
 import classes from '../styles/Top.module.css'
 
 export default function Top() {
+  const [posts, setPosts] = useState([]);
+
+  // APIでpostsを取得する処理をuseEffectで実行します。
+  useEffect(() => {
+    const loadPosts = async () => {
+      const res = await fetch("https://1hmfpsvto6.execute-api.ap-northeast-1.amazonaws.com/dev/posts"); // (例)郵便局に手紙を出す await fetch()
+      const data = await res.json(); // (例)返事を開けて内容を確認 await res.json()
+      setPosts(data.posts); // (例)返事の内容をノートに書き留める（状態に保存）
+    }
+
+    loadPosts();
+  }, []) //[]空でレンダリング時に一度だけ処理を発火
+
   return (
     <div>
       <ul className={classes.list}>
         {
-          posts.map(post => (
+          posts.map(post => ( // (post)でもよい。複数の場合は(post1, post2)など
             <li key={post.id} className={classes.listbox}>
               <Link to={'/report/' + post.id} className={classes.link}>
                 <div className={classes.posts}>
